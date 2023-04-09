081048b217733ead4e07c2742df8afb5781ed834
Pranjal Srivastava <75536136+iampsrv@users.noreply.github.com>
2023-04-09 19:44:17 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -12,6 +12,37 @@ resource "aws_instance" "dev_instance" {
   }
 }
 
+resource "aws_instance" "prince_instance" {
+  instance_type               = var.instance_type
+  ami                         = var.ami_id
+  count                       = var.number_of_instances
+  security_groups             = [aws_security_group.instance_security_group.id]
+  subnet_id                   = aws_subnet.dev_subnet.id
+  user_data                   = data.cloudinit_config.server_config.rendered
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "prince-instance"
+  }
+}
+
+
+resource "aws_instance" "rahul_instance" {
+  instance_type               = var.instance_type
+  ami                         = var.ami_id
+  count                       = var.number_of_instances
+  security_groups             = [aws_security_group.instance_security_group.id]
+  subnet_id                   = aws_subnet.dev_subnet.id
+  user_data                   = data.cloudinit_config.server_config.rendered
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "rahul-instance"
+  }
+}
+
+
+
 # Launch an EC2 instance in the Stage
 resource "aws_instance" "stage_instance" {
   instance_type   = var.instance_type
